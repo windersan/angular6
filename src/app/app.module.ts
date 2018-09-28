c348a11ec82b0fc6190f87210126c63ea6794b84
Devin Andres Salemi <devin.a.salemi@gmail.com>
2018-09-27 18:50:34 -0700

    add bootstrap, jquery, create-employee, list-employees

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CreateEmployeeComponent } from './employee/create-employee.component';
+import { ListEmployeesComponent } from './employee/list-employees.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateEmployeeComponent,
+    ListEmployeesComponent
   ],
   imports: [
     BrowserModule
